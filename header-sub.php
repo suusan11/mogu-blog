e2b79fdf1276d6f82f3d09afdc7d18627f3a302e
suusan11 <gawow11@gmail.com>
2019-01-29 17:32:39 -0800

    change file name

--- header-sub.php
+++ header-sub.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?php get_stylesheet_uri(); ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
     <title><?php bloginfo('name'); ?></title>
 
     <!--to use Adobe font-->
