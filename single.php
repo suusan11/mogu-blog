4785814df46466dd3bbec6bde5d9dd1014879fe0
suusan11 <gawow11@gmail.com>
2019-01-29 17:33:09 -0800

    add link img

--- single.php
+++ single.php
@@ -16,13 +16,17 @@
 
         <div class="container">
             <section class="article">
-                <img src="./images/sample.jpeg" alt="sample img">
+<!--                --><?php
+//                    if(has_post_thumbnail()) :
+//                        the_post_thumbnail('large');
+//                ?>
+<!--                --><?php //endif; ?>
                 <div class="article__header">
                     <h1 class="article-title"><?php the_title(); ?></h1>
                     <div class="article__header--flex">
                         <p class="article-date"><?php echo get_the_date(); ?></p>
-                        <?php the_category(','); ?>
-                        <img class="category-icon" src="./images/burger.svg" alt="icon burger">
+	                    <?php the_category(','); ?>
+                        <img class="category-icon" src="<?php echo get_template_directory_uri(); ?>/images/burger.svg" alt="icon burger">
                     </div>
                 </div>
                 <div class="article__main">
@@ -31,16 +35,14 @@
                 <div class="article__footer">
                     <a href="">
                         <div class="article__footer--flex prev">
-                            <?php previous_post_link(); ?>
-                            <img src="./images/arrow-left.svg" alt="arrow left">
+                            <img src="<?php previous_post_link(); ?><?php echo get_template_directory_uri(); ?>/images/arrow-left.svg" alt="arrow left">
                             <p>ザ・北米飯　ハンバーガー</p>
                         </div>
                     </a>
                     <a href="">
                         <div class="article__footer--flex next">
-                            <?php next_post_link(); ?>
                             <p>ザ・北米飯　ハンバーガー</p>
-                            <img src="./images/arrow-right.svg" alt="arrow right">
+                            <img src="<?php next_post_link(); ?><?php echo get_template_directory_uri(); ?>/images/arrow-right.svg" alt="arrow right">
                         </div>
                     </a>
                 </div>
