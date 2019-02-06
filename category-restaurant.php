d5f4d231b78047b2da318f327690ad503ea61660
suusan11 <gawow11@gmail.com>
2019-02-06 15:47:07 -0800

    add button to next page

---
+++ category-restaurant.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miiiiiiiiiiie
+ * Date: 2019-02-04
+ * Time: 17:16
+ */
--- index.php
+++ index.php
@@ -15,57 +15,40 @@
 
 <?php get_header(); ?>
 
+    <div class="container">
+        <section class="thumbnails">
+            <?php if(have_posts()):
+                while(have_posts()): the_post();?>
 
-<?php
-//    //サブクエリ発動　カテゴリ名よりもスラッグ名（'restaurant'）でサーチ
-//    $category_data = new WP_Query(array('category_name' => 'restaurant'));
-//
-//
-//    if ( $category_data->have_posts() ) {
-//        // The 2nd Loop
-//        while ( $category_data->have_posts() ) {
-//            $category_data->the_post();
-//
-//            echo '<li>' . get_the_title( $category_data->post->ID ) . '</li>';
-////            echo '<li>' . get_the_title( $category_data->post->ID ) ;  get_the_category($category_data->post->ID). '</li>';
-//            $category = get_the_category();
-//            print_r($category[0]->category_nicename);
-//        }
-//
-//        // Restore original Post Data　メインクエリに戻す→サブクエリ発動したら最後に書く
-//        wp_reset_postdata();
-//    }
-//
-//?>
+                <a href="<?php the_permalink(); ?>">
+                    <div class="thumbnail-container">
+                        <?php
+                        if(has_post_thumbnail()) :
+                            the_post_thumbnail('thumbnail');
+                            ?>
+                            <div class="thumbnail-info">
+                                <h1 class="article-title"><?php the_title(); ?></h1>
+                                <p class="article-date"><?php echo get_the_date(); ?></p>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                </a>
 
-        <div class="container">
-            <section class="thumbnails">
-                <?php if(have_posts()):
-                    while(have_posts()): the_post();?>
+                <?php endwhile;
+            else: ?>
+                <p>まだ食べてないよ！</p>
+            <?php endif;?>
 
-                    <a href="<?php the_permalink(); ?>">
-                        <div class="thumbnail-container">
-                            <?php
-                            if(has_post_thumbnail()) :
-                                the_post_thumbnail('thumbnail');
-                                ?>
-                                <div class="thumbnail-info">
-                                    <h1 class="article-title"><?php the_title(); ?></h1>
-                                    <p class="article-date"><?php echo get_the_date(); ?></p>
-                                </div>
-                            <?php endif; ?>
-                        </div>
-                    </a>
-
-                    <?php endwhile;
-                else: ?>
-                    <p>まだ食べてないよ！</p>
-                <?php endif;?>
-            </section>
-        </div>
-
-<?php if(the_post() > 10): ?>
-    <button><?php the_permalink(); ?>もっと見る</button>
-<?php endif;?>
+	        <?php
+	        if(!is_paged() && get_next_posts_link()) {
+		        echo '<button class="link__next-page">もっと見る</button>';
+	        }elseif(!is_paged() && !get_next_posts_link()) {
+	        }
+	        else {
+		        wp_link_pages();
+	        }
+	        ?>
+        </section>
+    </div>
 
 <?php get_footer(); ?>
