5440c0d8f5b33318b2cd25733e1012d10d86404d
suusan11 <gawow11@gmail.com>
2019-02-26 18:02:51 -0800

    add about section

--- index.php
+++ index.php
@@ -50,4 +50,19 @@
         </section>
     </div>
 
+    <div class="container">
+        <section class="intro">
+            <div class="intro-icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/pig.svg" alt="pig icon">
+            </div>
+            <div class="intro-flex">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/me.jpeg" alt="my img">
+                <p>カナダ・バンクーバー在住、食べることが大好きなべーぐるです。<br>
+                    一番好きな食べ物は餃子、カフェでよく注文する飲み物はロンドンフォグ、好きな言葉は"お腹いっぱい"です。
+                    某百貨店の食料品売り場を5年半担当し培われた食欲と胃袋が自慢です。独断と偏見、そして偏食によるカナダの食情報をお届けします。
+                </p>
+            </div>
+        </section>
+    </div>
+
 <?php get_footer(); ?>
