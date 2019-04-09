2cf381c66c8c8280a180f3ef30223c69b07d0ec4
suusan11 <gawow11@gmail.com>
2019-04-09 15:19:32 -0700

    fix about part of top page

--- index.php
+++ index.php
@@ -52,15 +52,23 @@
 
     <div class="container">
         <section class="intro">
-            <div class="intro-icon">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/pig.svg" alt="pig icon">
+            <div class="intro__icon">
+                <img src="<?php echo get_template_directory_uri()?>/images/pig.svg" alt="pig icon">
             </div>
-            <div class="intro-flex">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/me.jpeg" alt="my img">
-                <p>カナダ・バンクーバー在住、食べることが大好きなべーぐるです。<br>
-                    一番好きな食べ物は餃子、カフェでよく注文する飲み物はロンドンフォグ、好きな言葉は"お腹いっぱい"です。
-                    某百貨店の食料品売り場を5年半担当し培われた食欲と胃袋が自慢です。独断と偏見、そして偏食によるカナダの食情報をお届けします。
-                </p>
+            <div class="intro__flex">
+                <img src="<?php echo get_template_directory_uri()?>/images/me.jpeg" alt="my img">
+                <div>
+                    <p>カナダ・バンクーバー在住、食べることが大好きな私です。
+                        一番好きな食べ物は餃子、カフェでよく注文する飲み物はロンドンフォグ、好きな言葉は"お腹いっぱい"です。
+                        某百貨店の食料品売り場を5年半担当し培われた食欲と胃袋が自慢です。独断と偏見、そして偏食によるカナダの食情報をお届けします。
+                    </p>
+                    <div class="intro__flex-insta">
+                        <a href="https://www.instagram.com/miiiiiiiiiiie.55/?hl=ja">
+                            <img src="<?php echo get_template_directory_uri()?>/images/insta.png" alt="instagram icon">
+                        </a>
+                        <p><< follow me!!</p>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
