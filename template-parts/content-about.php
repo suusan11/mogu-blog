60add997d4d83b93c6b23c983d4ad0313c1df6d2
suusan11 <gawow11@gmail.com>
2019-04-16 15:54:08 -0700

    add target to link

--- template-parts/content-about.php
+++ template-parts/content-about.php
@@ -20,7 +20,7 @@
                     某百貨店の食料品売り場を5年半担当し培われた食欲と胃袋が自慢です。独断と偏見、そして偏食によるカナダの食情報をお届けします。
                 </p>
                 <div class="intro__flex-insta">
-                    <a href="https://www.instagram.com/miiiiiiiiiiie.55/?hl=ja">
+                    <a href="https://www.instagram.com/miiiiiiiiiiie.55/?hl=ja" target="_blank">
                         <img src="<?php echo get_template_directory_uri()?>/images/insta.png" alt="instagram icon">
                     </a>
                     <p><< follow me!!</p>
