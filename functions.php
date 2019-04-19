6f4f517e1232bc8f846110b0e089c454b68beeb7
suusan11 <gawow11@gmail.com>
2019-04-18 23:52:06 -0700

    change prev and next text

--- functions.php
+++ functions.php
@@ -36,8 +36,8 @@ function the_pagination() {
 		'format'       => '',
 		'current'      => max( 1, get_query_var('paged') ),
 		'total'        => $wp_query->max_num_pages,
-		'prev_text'    => '&larr;',
-		'next_text'    => '&rarr;',
+		'prev_text'    => '<<',
+		'next_text'    => '>>',
 		'type'         => 'list',
 		'end_size'     => 3,
 		'mid_size'     => 3
