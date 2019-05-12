a41ba9c47d0603f2a6921dcdd63aae293890a167
suusan11 <gawow11@gmail.com>
2019-05-12 14:44:30 -0700

    modify hero img

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
 <body>
 
     <div class="hero">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/hero.jpg" alt="hero img">
+<!--        <img src="--><?php //echo get_template_directory_uri(); ?><!--/images/hero.jpg" alt="hero img">-->
     </div>
     <!--end hero image-->
 
